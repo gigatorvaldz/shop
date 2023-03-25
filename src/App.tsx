3c6607c8ea7b32da96ed8f5e3aadd2bc9a41d106
gigatorvaldz <ltorvaldz@mail.ru>
2023-03-25 14:50:40 +0300

    App refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
 import CataloguePage from './Pages/CataloguePage';
 import "./SCSS/App.scss"
+
+// [Notes]:
+
+// Loader, Error Boundary components (Utils)
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      {/* <Header/> */}
       <CataloguePage/>
+      {/* <Footer/> */}
 
     </div>
   );
