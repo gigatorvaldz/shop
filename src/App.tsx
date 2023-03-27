78e848543e7c85119d63c1df952101cbe1e5444b
Artyom <ltorvaldz@mail.ru>
2023-03-27 21:10:12 +0300

    Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
-import Header from './Components/Header/Header';
-import Footer from './Components/Footer/Footer';
-import CataloguePage from './Pages/CataloguePage';
-import "./SCSS/App.scss"
+import React from "react";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import CataloguePage from "./Pages/CataloguePage";
+import "./SCSS/App.scss";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import ErrorPage from "./Pages/ErrorPage";
+import PostPage from "./Pages/PostPage";
+import Breadcrumbs from "./Components/Utils/Breadcrumbs/Breadcrumbs";
 
 // [Notes]:
 
-// Loader, Error Boundary components (Utils)
+// Loader, Error Boundary, Breadcrumbs components (Utils)
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <CataloguePage/>
-      <Footer/>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/catalogue" element={<CataloguePage />} />
+          <Route path="/post/:id" element={<PostPage />} />
+          <Route path="/*" element={<ErrorPage />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
